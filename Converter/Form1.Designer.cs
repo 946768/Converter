4593949208aa153c79a97afbda99da187b88d560
medvedevaJ <jule4ka03@mail.ru>
2022-04-18 19:23:44 +0300

    Добавьте файлы проекта.

---
+++ Converter/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Converter
+{
+    partial class Конвектер
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.BackColor = System.Drawing.Color.Transparent;
+            this.ConvertButton.BackgroundImage = global::Converter.Properties.Resources.png_transparent_computer_icons_data_conversion_religious_conversion_concert_miscellaneous_trademark_logo;
+            this.ConvertButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ConvertButton.Location = new System.Drawing.Point(177, 64);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(124, 124);
+            this.ConvertButton.TabIndex = 0;
+            this.ConvertButton.UseVisualStyleBackColor = false;
+            this.ConvertButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(28, 206);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(130, 27);
+            this.textBoxFrom.TabIndex = 1;
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(319, 206);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(138, 27);
+            this.textBoxTo.TabIndex = 2;
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Items.AddRange(new object[] {
+            "USD",
+            "EUR",
+            "RUB",
+            "KZT"});
+            this.comboBoxFrom.Location = new System.Drawing.Point(28, 27);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(130, 28);
+            this.comboBoxFrom.TabIndex = 3;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Items.AddRange(new object[] {
+            "USD",
+            "EUR",
+            "RUB",
+            "KZT"});
+            this.comboBoxTo.Location = new System.Drawing.Point(319, 27);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(130, 28);
+            this.comboBoxTo.TabIndex = 4;
+            // 
+            // Конвектер
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Converter.Properties.Resources.scale_1200_3_4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(471, 292);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.ConvertButton);
+            this.Name = "Конвектер";
+            this.Text = "Конвектер ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+    }
+}
+
---
+++ Converter/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Конвектер: Form
+    {
+        public Конвектер()
+
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxFrom.Text == "")
+            {
+                MessageBox.Show("Введите валюту!");
+            }
+            else
+            {
+
+
+                double kzt = 0.0, eur = 0.0, rub = 0.0, usd = 0.0, from;
+                from = Double.Parse(textBoxFrom.Text);
+
+                string operatorFrom = comboBoxFrom.Text;
+                string operatorTo = comboBoxTo.Text;
+
+                switch (operatorFrom)
+                {
+                    case "USD":
+                        kzt = 449.64;
+                        eur = 0.91979;
+                        rub = 79.63;
+                        usd = 1.0;
+                        break;
+                    case "EUR":
+                        kzt = 490.06;
+                        eur = 1.0;
+                        rub = 86.79;
+                        usd = 1.09;
+                        break;
+                    case "RUB":
+                        kzt = 5.65;
+                        eur = 0.011522;
+                        rub = 1.0;
+                        usd = 0.012558;
+                        break;
+                    case "KZT":
+                        kzt = 1.0;
+                        eur = 0.002039;
+                        rub = 0.1771;
+                        usd = 0.002224;
+                        break;
+                }
+                switch (operatorTo)
+                {
+                    case "USD":
+                        textBoxTo.Text = Convert.ToString(from * usd);
+                        break;
+                    case "EUR":
+                        textBoxTo.Text = Convert.ToString(from * eur);
+                        break;
+                    case "RUB":
+                        textBoxTo.Text = Convert.ToString(from * rub);
+                        break;
+                    case "KZT":
+                        textBoxTo.Text = Convert.ToString(from * kzt);
+                        break;
+                }
+            }
+
+        }
+
+        
+
+    }
+}
